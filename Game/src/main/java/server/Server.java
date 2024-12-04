5f907ff7d6fdbdee4206bc991d8bf235e9623c32
Anna Gr <279694@student.pwr.edu.pl>
2024-12-04 19:33:31 +0100

    1st version of client-server

---
+++ Game/src/main/java/server/Server.java
@@ -0,0 +1,28 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(4444)) {
+
+            System.out.println("Server is listening on port 4444");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                new Mediator(socket).start();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
+
